d303da3d657a2a656283a3b2650a43c2a552e217
1041508314 <1041508314@qq.com>
2020-05-08 15:12:37 +0800

    发送代码

---
+++ Double linked list/src/TestDemo.java
@@ -0,0 +1,28 @@
+/**
+ * @program: Double linked list
+ * @description
+ * @author: LiuXinYu
+ * @create: 2020-05-06 15:38
+ **/
+public class TestDemo {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addLast(1);
+        myLinkedList.addLast(2);
+        myLinkedList.addLast(3);
+        myLinkedList.addLast(4);
+        myLinkedList.addLast(5);
+//        myLinkedList.display();
+//        myLinkedList.addIndex(0,99);
+//        myLinkedList.display();
+//        myLinkedList.addIndex(5,199);
+//        myLinkedList.display();
+//        myLinkedList.addIndex(2,9999);
+//        myLinkedList.display();
+//        myLinkedList.remove(199);
+//        myLinkedList.removeAllkey(2);
+//        myLinkedList.display();
+//        System.out.println(myLinkedList.contains(4));
+//        System.out.println(myLinkedList.size());
+    }
+}
